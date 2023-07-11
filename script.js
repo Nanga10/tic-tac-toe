4d4704b6ed02b23e16c55d41b8b1daec684d2c5e
Nangamso <na.tshwete@gmail.com>
2023-07-11 22:32:54 +1000

    restart game functionality working

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ const gameBoard = ["", "", "", "", "", "", "", "", ""];
 let currentPlayer;
 const startBtn = document.getElementById("start-game");
 
+const titleEl = document.createElement("h1");
+titleEl.textContent = "Tic Tac Toe";
+titleEl.classList.add("title");
+document.body.appendChild(titleEl);
+
+const subTitleEl = document.createElement("p");
+subTitleEl.textContent = "Select a square from the game board to begin";
+subTitleEl.classList.add("sub-title");
+document.body.appendChild(subTitleEl);
+
 const Gameboard = (() => {
   //game board arrow function
   const makeGameBoardElem = () => {
@@ -18,15 +28,25 @@ const Gameboard = (() => {
     return squareElement;
   };
 
-  gameBoardElem = makeGameBoardElem();
-  for (let square = 0; square < 9; square++) {
-    gameBoardElem.appendChild(makeSquareElem(square));
-  }
+  const render = () => {
+    if (gameBoardElem) {
+      // check if old gameboard exists
+      document.body.removeChild(gameBoardElem); // remove old gameboard
+    }
+    gameBoardElem = makeGameBoardElem();
+    for (let square = 0; square < 9; square++) {
+      gameBoardElem.appendChild(makeSquareElem(square));
+    }
+    currentPlayer = [0]; //initialize current player
+    return document.body.appendChild(gameBoardElem);
+  };
 
-  currentPlayer = [0]; //Initialize player
-
-  document.body.appendChild(gameBoardElem);
+  return {
+    render,
+  };
 })(); //IIFE
+
+Gameboard.render(); //calling render game funtion inside Gameboard
 
 //creating multiple players therefore using a factory to create players
 const createPlayer = (marker) => {
@@ -39,19 +59,17 @@ const createPlayer = (marker) => {
 const Game = (() => {
   let squareEl = document.getElementsByClassName("game-square"); // returns an array of all ements with specified class
   let players = [];
-  let currentPlayer = 0;
+  let currentPlayer = "O";
   let gameOver;
 
   players = [createPlayer("X"), createPlayer("O")]; // create player objects
-
-  gameOver = false;
 
   for (let i = 0; i < squareEl.length; i++) {
     squareEl[i].addEventListener(
       "click",
       (event) => {
         const { target } = event;
-        target.textContent = currentPlayer; //write current player marker square when pressed
+        target.textContent = currentPlayer; //draw current player marker square when pressed
         let squareElId = squareEl[i].id; // storing id  for specific square in squareElId
         const substring = squareElId.split("-", 1); // split id string into two strings, keeping 1 string
         gameBoard[substring] = currentPlayer; //play marker in indexed position gameBoard array
@@ -93,8 +111,7 @@ const Game = (() => {
         gameBoard[position1] === gameBoard[position2] &&
         gameBoard[position1] === gameBoard[position3]
       ) {
-        console.log(`${gameBoard[position1]} player Wins!`);
-        // completeGame(`${gameBoard[position1]} player Wins!`);
+        completeGame(`${gameBoard[position1]} player Wins!`);
       }
     }
 
@@ -108,35 +125,20 @@ const Game = (() => {
 
 const completeGame = (message) => {
   const overlayElem = document.createElement("div");
-  overlayElem.style.position = "fixed";
-  overlayElem.style.top = "0";
-  overlayElem.style.left = "0";
-  overlayElem.style.bottom = "0";
-  overlayElem.style.right = "0";
-  overlayElem.style.backgroundColor = "rgba(0,0,0,0.8)";
-  overlayElem.style.display = "flex";
-  overlayElem.style.flexDirection = "column";
-  overlayElem.style.justifyContent = "center";
-  overlayElem.style.alignItems = "center";
-  overlayElem.style.textAlign = "center";
+  overlayElem.classList.add("overlay");
 
   const messageElem = document.createElement("h2");
   messageElem.textContent = message;
-  messageElem.style.color = "white";
-  messageElem.style.fontSize = "100px";
-
+  messageElem.classList.add("game-over-message");
   overlayElem.appendChild(messageElem);
 
   const restartButtonElem = document.createElement("button");
+  restartButtonElem.classList.add("restart-button");
   restartButtonElem.textContent = "Restart";
-  restartButtonElem.style.backgroundColor = "transparent";
-  restartButtonElem.style.color = "white";
-  restartButtonElem.style.border = "1px solid white";
-  restartButtonElem.style.padding = "10px 30px";
 
   restartButtonElem.addEventListener("click", () => {
-    resetGame();
     document.body.removeChild(overlayElem);
+    Gameboard.render();
   });
 
   overlayElem.appendChild(restartButtonElem);
